90613b9acc7d4fa2a0a812dab7a68f04f9941615
Daljja <98799323+codenamesu@users.noreply.github.com>
2022-10-13 17:57:02 +0900

    [C#] - Project 생성

---
+++ DbManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Data
+{
+    internal class DbManager
+    {
+        private static DbManager Instance = null;
+
+        private static 
+    }
+}
